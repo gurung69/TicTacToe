b155ec4632feb619e19127ce6de56d64d4d490d3
gurung69 <gurungkrish44@gmail.com>
2024-02-17 14:18:19 -0600

    made grid responsive to click and draw on the clicked positin based on player/Users/gurungkrish/opt/anaconda3/bin/python /Users/gurungkrish/Desktop/tictactoe/tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -12,7 +12,7 @@ class TicTacToe:
     self.win = Tk()
     self.canvas = Canvas(self.win, width=size, height=size)
     self.canvas.pack()
-    
+    self.canvas.bind("<Button-1>", self.fill_position)
 
     self.board = np.zeros((3,3))
 
@@ -23,23 +23,25 @@ class TicTacToe:
     for i in range(1,3): # create grid
       self.canvas.create_line(i*size/3, 0, i*size/3, size)
       self.canvas.create_line(0, i*size/3, size, i*size/3)
+  
+  def fill_position(self, event):
+    position = np.array([event.x, event.y])
+    position = (position//(size/3)).astype(int)
 
+    if self.curr_player == "X":
+      self.canvas.create_line((size/3)*position[0]+symbol_width, (size/3)*position[1]+symbol_width, (size/3)*position[0]+(size/3)-symbol_width, (size/3)*position[1]+(size/3)-symbol_width, width=symbol_width)
+      self.canvas.create_line((size/3)*position[0]+(size/3)-symbol_width, (size/3)*position[1]+symbol_width, (size/3)*position[0]+symbol_width, (size/3)*position[1]+(size/3)-symbol_width, width=symbol_width)
+    if self.curr_player == "O":
+      self.canvas.create_oval((size/3)*position[0]+symbol_width, (size/3)*position[1]+symbol_width, (size/3)*position[0]+(size/3)-symbol_width, (size/3)*position[1]+(size/3)-symbol_width, width=symbol_width)
+
+    self.switch_player()
+  
+  def switch_player(self):
+    if self.curr_player == "X":
+      self.curr_player = "O"
+    else:
+      self.curr_player = "X"
+    
 
 
 game = TicTacToe()
-    
-
-# def clicked(event):
-#   print(event.x, event.y)
-
-# win = Tk()
-# canvas = Canvas(win, width=size, height=size)
-# canvas.bind("<Button-1>", clicked)
-
-
-
-# for i in range(1,3): # create grid
-#   canvas.create_line(i*size/3, 0, i*size/3, size)
-#   canvas.create_line(0, i*size/3, size, i*size/3)
-# canvas.pack()
-# win.mainloop()
